94333c778f48808332b36bbe219daccdb112273e
ngoeldner <ngoeldner@hotmail.com>
2021-12-03 17:01:21 -0300

    removed launch file

--- src/arduino_interface/launch/crawler_launch.py
+++ src/arduino_interface/launch/crawler_launch.py
@@ -1,35 +1,35 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
 
-def generate_launch_description():
-    ld = LaunchDescription()
-    arduino_node = Node(
-        package='arduino_interface',
-        executable='arduino_pub',
-    )
+# def generate_launch_description():
+#     ld = LaunchDescription()
+#     arduino_node = Node(
+#         package='arduino_interface',
+#         executable='arduino_pub',
+#     )
 
-    encoder_node = Node(
-        package='encoder_interface',
-        executable='encoder_pub',
-    )
+#     encoder_node = Node(
+#         package='encoder_interface',
+#         executable='encoder_pub',
+#     )
 
-    imu_node = Node(
-        package='imu_interface',
-        executable='imu_pub',
-    )
-    # cam_node = Node(
-    #     package='usb_cam',
-    #     executable='usb_cam_node_exe'
-    # )
-    gps_node = Node(
-        package='gps_interface',
-        executable='gps_pub',
-    )
+#     imu_node = Node(
+#         package='imu_interface',
+#         executable='imu_pub',
+#     )
+#     # cam_node = Node(
+#     #     package='usb_cam',
+#     #     executable='usb_cam_node_exe'
+#     # )
+#     gps_node = Node(
+#         package='gps_interface',
+#         executable='gps_pub',
+#     )
 
-    ld.add_action(arduino_node)
-    ld.add_action(encoder_node)
-    ld.add_action(imu_node)
-    ld.add_action(gps_node)
+#     ld.add_action(arduino_node)
+#     ld.add_action(encoder_node)
+#     ld.add_action(imu_node)
+#     ld.add_action(gps_node)
 
-    return ld
+#     return ld
     
